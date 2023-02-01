ad70ac8f80f9caa33ea17060db9a0a9c55314818
Angelo <angelo@rcstart.it>
2023-02-01 01:03:15 +0100

    bitsong code refactor * remove original contracts * add `uri` to bs721 `ContractInfo` * generate new schema

---
+++ packages/bs721/src/lib.rs
@@ -0,0 +1,15 @@
+mod msg;
+mod query;
+mod receiver;
+mod traits;
+
+pub use cw_utils::Expiration;
+
+pub use crate::msg::Bs721ExecuteMsg;
+pub use crate::query::{
+    AllNftInfoResponse, Approval, ApprovalResponse, ApprovalsResponse, ContractInfoResponse,
+    Bs721QueryMsg, NftInfoResponse, NumTokensResponse, OperatorsResponse, OwnerOfResponse,
+    TokensResponse,
+};
+pub use crate::receiver::Bs721ReceiveMsg;
+pub use crate::traits::{Bs721, Bs721Execute, Bs721Query};
